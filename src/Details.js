e95eea126339ba576b67bb1f2bb01edeadb63197
Naveena V <60342603+Naveena-V@users.noreply.github.com>
2024-08-10 17:03:04 +0530

    portfolio changes

---
+++ src/Details.js
@@ -0,0 +1,143 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Naveena V",
+  tagline: "I build things for web and Mobile",
+  img: profile,
+  about:
+    "I’m looking for a role where I can apply my MERN stack skills to build innovative web applications in a collaborative, growth-focused environment. I’m excited to tackle challenging projects, work with cutting-edge technologies, and contribute to a team that values creativity and continuous learning.",
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/naveena-v-8042931a2/",
+  github: "https://github.com/Naveena-V",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Senior Software Engineer",
+    Company: `SLK Softwares`,
+    Location: "Bengaluru",
+    Type: "Full Time",
+    Duration: "Jun 2023 - Present",
+  },
+  {
+    Position: "Software Developer",
+    Company: `Global Health Care Billing Partners Pvt Ltd`,
+    Location: "Chennai",
+    Type: "Full Time",
+    Duration: "Feb 2022 - Jun 2023",
+  },
+  {
+    Position: "Software Developer",
+    Company: `Dewhive Solutions`,
+    Location: "Bengaluru",
+    Type: "Full Time",
+    Duration: "Nov 2020 - Feb 2022",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Bachelor of Engineering (Computer Science)",
+    Company: " MVJ College of Engineering",
+    Location: "Bengaluru",
+    Type: "Full Time",
+    Duration: "June 2016 - Jul 2019",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "DMP Flow",
+    image: projectImage1,
+    description: `DMP Flow is an eCommerce platform for process applications where users can specify
+      fluid, gas, and steam meters specifications, purchase products, and receive
+      recommendations of products based on their inputs.`,
+    techstack: "ReactJS, Redux, Typescript, NodeJS, ExpressJS, Bootstrap",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "CLAIMO Portal",
+    image: projectImage2,
+    description: `CLAIMO Portal empowers doctors to efficiently manage patient insurance claims and
+      monitor claim statuses in real-time. The portal provides an intuitive interface for handling documentation, reduces administrative burdens,`,
+    techstack: "ReactJS, Redux/Toolkit, Javascript, Typescript, MaterialUi",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "HEALTHCONNECT Advisor",
+    image: projectImage3,
+    description: `HEALTHCONNECT is interactive platform connecting patients and doctors, eal-time consultations, secure messaging, and appointment scheduling, i
+    mproving patient access and streamlining healthcare services.`,
+    techstack: "ReactJS, Redux, Typescript, NodeJS, ExpressJS, Bootstrap",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "naveena6677navi@gmail.com",
+  phone: "+91 97315 10950",
+};
